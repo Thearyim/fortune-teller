dfd9f591094485891de30a4b15051c0dc715d2b4
Theary Im and Yulia Shidlovskaya <theary@>
2019-01-23 11:18:33 -0800

    initialize index.html

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $("#formOne").submit(function(event) {
+
+    var inputText = $("input#text").val();
+    var outputText = "";
+
+
+    event.preventDefault();
+
+  });
+});
