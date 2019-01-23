54a3a7ae79853b0fba1b210e2ceae67d0cfd2af1
Theary Im and Yulia Shidlovskaya <yulia@>
2019-01-23 13:24:47 -0800

    Add checkbox logic.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,42 @@ $(document).ready(function(){
 
   $("#formOne").submit(function(event) {
 
-    var inputText = $("input#text").val();
-    var outputText = "";
+    $("#results").show();
 
+    var luckyEvent = 0;
+    var unLuckyEvent = 0;
+
+    $("input:checkbox[name=luck]:checked").each(function() {
+      var luckValue = $(this).val();
+      luckyEvent++;
+      $('.goodLuckResults').append(luckValue + "<br>");
+    });
+
+    $("input:checkbox[name=unLuck]:checked").each(function() {
+      var unLuckValue = $(this).val();
+      unLuckyEvent++;
+      $('.badLuckResults').append(unLuckValue + "<br>");
+    });
+
+    if (luckyEvent > unLuckyEvent) {
+      $('.goodLuckResults').show();
+      $('.badLuckResults').hide();
+    }
+    else if (luckyEvent < unLuckyEvent) {
+      $('.badLuckResults').show();
+      $('.goodLuckResults').hide();
+    }
+    else if (luckyEvent === unLuckyEvent) {
+      $('.nutralResults').show();
+      $('.badLuckResults').show();
+      $('.goodLuckResults').show();
+    }
 
     event.preventDefault();
 
+  });
+
+  $("#goBack").click(function(){
+    location.reload();
   });
 });
